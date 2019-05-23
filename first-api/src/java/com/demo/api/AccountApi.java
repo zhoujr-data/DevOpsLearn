65ecf523908ea162e0af8d0bc425eba60a25695a
zhoujr <zhoujingren@ec.e-chemchina.com>
2019-05-23 16:31:14 +0800

    修改

---
+++ first-api/src/java/com/demo/api/AccountApi.java
@@ -0,0 +1,35 @@
+package java.com.demo.api;
+
+
+
+public interface AccountApi {
+	/**
+	 * 查询
+	 * @param req 分页查询
+	 */
+
+
+	/**
+	 * 详细
+	 * @param req 查询
+	 */
+
+	/**
+	 * 保存
+	 * @param req 保存条件
+	 */
+
+	/**
+	 * 删除
+	 * @param req 删除条件
+	 */
+
+	/**
+	 * 删除
+	 * @param req 删除条件
+	 */
+
+	/**
+	 * 根据sellerId获得详细信息
+	 */
+}
